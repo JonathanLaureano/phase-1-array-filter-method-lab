b96df6cdada5f47b9615e822f06014dbbb4f07f9
JonathanLaureano <JonathanLaureano20@gmail.com>
2022-01-11 14:24:00 -0500

    commit tree d88a415a50e14a510b3ed663c772d3cc3c5c790b

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching (drivers, name) {
+     return drivers.filter(driver => {if (driver.toLowerCase() === name.toLowerCase()){
+        return driver; 
+     }})
+}
+function fuzzyMatch (drivers, letters) {
+   return drivers.filter(driver => {if (driver.indexOf(letters) === 0){
+      return driver;
+   }})
+}
+function matchName (drivers, name) {
+   return drivers.filter( driver => driver.name === name)
+}
